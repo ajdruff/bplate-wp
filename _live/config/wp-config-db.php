7da63ee3803464a15fa0501214cac8fdbbd2877b
Andrew Druffner <andrew@nomstock.comexit>
2015-02-21 13:57:22 -0800

    added back in _live and _stage.

---
+++ _live/config/wp-config-db.php
@@ -0,0 +1,14 @@
+<?php
+	
+define('DB_NAME', 'YOUR_DB_NAME');
+
+/** MySQL database username */
+define('DB_USER', 'YOUR_DB_USER');
+
+/** MySQL database password */
+define('DB_PASSWORD', 'YOUR_DB_USER_PASSWORD');
+
+/** MySQL hostname */
+define('DB_HOST', 'localhost');
+
+?>
